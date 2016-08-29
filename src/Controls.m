e5ba5d3bedce958cdc8c662ac1426fdfb69821b3
Grant Paul <git@grantpaul.com>
2016-08-29 01:32:09 -0700

    Add customized control layouts for two games.

--- src/Controls.m
+++ src/Controls.m
@@ -9,11 +9,76 @@
 #import "Control.h"
 #import "ControlViewController.h"
 
-static UIWindow *window = nil;
+static ControlViewController *UndertaleLayout(void)
+{
+    CGSize actionSize = CGSizeMake(70, 70);
+    CGPoint actionAnchor = CGPointMake(0.85, 0.8);
+    NSArray<Button *> *action = @[
+        [[Button alloc] initWithName:@"Z" keycode:input_keycode_z center:CGVectorMake(45, -20) anchor:actionAnchor size:actionSize],
+        [[Button alloc] initWithName:@"X" keycode:input_keycode_x center:CGVectorMake(-45, 20) anchor:actionAnchor size:actionSize],
+    ];
+
+    CGSize controlSize = CGSizeMake(75, 25);
+    CGPoint controlAnchor = CGPointMake(0.5, 0.92);
+    NSArray<Button *> *control = @[
+        [[Button alloc] initWithName:@"CONTROL" keycode:input_keycode_control center:CGVectorMake(0, 0) anchor:controlAnchor size:controlSize],
+    ];
+
+    NSMutableSet<Button *> *buttons = [NSMutableSet set];
+    [buttons addObjectsFromArray:action];
+    [buttons addObjectsFromArray:control];
+
+    CGSize padSize = CGSizeMake(180, 180);
+    CGPoint padAnchor = CGPointMake(0.15, 0.75);
+    Pad *pad = [[Pad alloc] initWithUpKeycode:input_keycode_up leftKeycode:input_keycode_left downKeycode:input_keycode_down rightKeycode:input_keycode_right center:CGVectorMake(0, 0) anchor:padAnchor size:padSize];
+    NSSet<Pad *> *pads = [NSSet setWithObject:pad];
+
+    return [[ControlViewController alloc] initWithButtons:buttons pads:pads];
+}
+
+static ControlViewController *AM2RLayout(void)
+{
+    CGSize actionSize = CGSizeMake(70, 70);
+    CGPoint actionAnchor = CGPointMake(0.85, 0.8);
+    NSArray<Button *> *action = @[
+        [[Button alloc] initWithName:@"Z" keycode:input_keycode_z center:CGVectorMake(45, -20) anchor:actionAnchor size:actionSize],
+        [[Button alloc] initWithName:@"X" keycode:input_keycode_x center:CGVectorMake(-45, 20) anchor:actionAnchor size:actionSize],
+    ];
+
+    CGSize triggerSize = CGSizeMake(110, 35);
+    CGFloat triggerOffset = -5;
+    NSArray<Button *> *trigger = @[
+        [[Button alloc] initWithName:@"SHIFT" keycode:input_keycode_shift center:CGVectorMake(triggerSize.width / 2 - triggerOffset, triggerSize.height / 2 - triggerOffset) anchor:CGPointMake(0.0, 0.0) size:triggerSize],
+        [[Button alloc] initWithName:@"A" keycode:input_keycode_a center:CGVectorMake(-triggerSize.width / 2 + triggerOffset, triggerSize.height / 2 - triggerOffset) anchor:CGPointMake(1.0, 0.0) size:triggerSize],
+    ];
+
+    CGSize controlSize = CGSizeMake(75, 25);
+    CGPoint controlAnchor = CGPointMake(0.5, 0.92);
+    NSArray<Button *> *control = @[
+        [[Button alloc] initWithName:@"ENTER" keycode:input_keycode_enter center:CGVectorMake(-45, 0) anchor:controlAnchor size:controlSize],
+        [[Button alloc] initWithName:@"S" keycode:input_keycode_s center:CGVectorMake(45, 0) anchor:controlAnchor size:controlSize],
+    ];
+
+    NSMutableSet<Button *> *buttons = [NSMutableSet set];
+    [buttons addObjectsFromArray:action];
+    [buttons addObjectsFromArray:trigger];
+    [buttons addObjectsFromArray:control];
+
+    CGSize padSize = CGSizeMake(180, 180);
+    CGPoint padAnchor = CGPointMake(0.15, 0.75);
+    Pad *pad = [[Pad alloc] initWithUpKeycode:input_keycode_up leftKeycode:input_keycode_left downKeycode:input_keycode_down rightKeycode:input_keycode_right center:CGVectorMake(0, 0) anchor:padAnchor size:padSize];
+    NSSet<Pad *> *pads = [NSSet setWithObject:pad];
+
+    return [[ControlViewController alloc] initWithButtons:buttons pads:pads];
+}
+
 
 __attribute__((constructor))
 static void ControlsInitialize(void)
 {
+    /* Keep the window alive. */
+    static UIWindow *window = nil;
+
     NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
     NSOperationQueue *mainQueue = [NSOperationQueue mainQueue];
     [notificationCenter addObserverForName:UIApplicationDidFinishLaunchingNotification object:nil queue:mainQueue usingBlock:^(NSNotification *notification) {
@@ -21,34 +86,9 @@ static void ControlsInitialize(void)
             UIScreen *mainScreen = [UIScreen mainScreen];
 
             window = [[UIWindow alloc] initWithFrame:mainScreen.bounds];
-            window.windowLevel = 2000.0;
+            window.windowLevel = 1.0;
 
-            CGSize actionSize = CGSizeMake(65, 65);
-            CGPoint actionAnchor = CGPointMake(0.83, 0.7);
-            NSArray<Button *> *action = @[
-                [[Button alloc] initWithName:@"Z" keycode:input_keycode_z center:CGVectorMake(65, 0) anchor:actionAnchor size:actionSize],
-                [[Button alloc] initWithName:@"X" keycode:input_keycode_x center:CGVectorMake(0, 55) anchor:actionAnchor size:actionSize],
-                [[Button alloc] initWithName:@"A" keycode:input_keycode_a center:CGVectorMake(-65, 0) anchor:actionAnchor size:actionSize],
-                [[Button alloc] initWithName:@"C" keycode:input_keycode_c center:CGVectorMake(0, -55) anchor:actionAnchor size:actionSize],
-            ];
-
-            CGSize controlSize = CGSizeMake(75, 25);
-            CGPoint controlAnchor = CGPointMake(0.5, 0.92);
-            NSArray<Button *> *control = @[
-                [[Button alloc] initWithName:@"ENTER" keycode:input_keycode_enter center:CGVectorMake(-45, 0) anchor:controlAnchor size:controlSize],
-                [[Button alloc] initWithName:@"SHIFT" keycode:input_keycode_shift center:CGVectorMake(45, 0) anchor:controlAnchor size:controlSize],
-            ];
-
-            NSMutableSet<Button *> *buttons = [NSMutableSet set];
-            [buttons addObjectsFromArray:action];
-            [buttons addObjectsFromArray:control];
-
-            CGSize padSize = CGSizeMake(180, 180);
-            CGPoint padAnchor = CGPointMake(0.15, 0.70);
-            Pad *pad = [[Pad alloc] initWithUpKeycode:input_keycode_up leftKeycode:input_keycode_left downKeycode:input_keycode_down rightKeycode:input_keycode_right center:CGVectorMake(0, 0) anchor:padAnchor size:padSize];
-            NSSet<Pad *> *pads = [NSSet setWithObject:pad];
-
-            ControlViewController *viewController = [[ControlViewController alloc] initWithButtons:buttons pads:pads];
+            ControlViewController *viewController = UndertaleLayout();
             window.rootViewController = viewController;
 
             [window makeKeyAndVisible];
